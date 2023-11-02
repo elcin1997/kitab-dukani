3f4742501159c3e42eacdf22229a9a90c3175ca1
elcin1997 <109592202+elcin1997@users.noreply.github.com>
2023-11-02 12:23:25 +0400

    ilkhisse

---
+++ kitab-dukani/src/main/java/com/example/kitabdukani/KitabDukaniApplication.java
@@ -0,0 +1,13 @@
+package com.example.kitabdukani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitabDukaniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KitabDukaniApplication.class, args);
+    }
+
+}
---
+++ kitab-dukani/src/test/java/com/example/kitabdukani/KitabDukaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kitabdukani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitabDukaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
